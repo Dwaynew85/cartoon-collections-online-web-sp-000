4e7eb92c854f2727c60eca683b7117d0a7b4cfd9
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-06 01:44:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.collect do |name|
+    index = names.index(name) + 1
+    puts "#{index} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
