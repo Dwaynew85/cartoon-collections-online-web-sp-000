4e95d23b4f7e74c223b7e62bd95003aaf4dd898c
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-04-06 02:11:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,19 @@ def summon_captain_planet(names)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    if call.length > 4
+      true
+    else
+      false
+    end
+  end
 end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
